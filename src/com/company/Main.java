40c9a09d9a80def576217003eb7522f21764c82b
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-09-15 18:41:53 +0900

    해커랭크 2단계 추가

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,65 @@
 package com.company;
 
-import java.io.*;
+import com.company.hakerank.two.BigDecimal;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        int n, row, val;
+        n = scanner.nextInt();
+
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+
+        //list.add(new Integer[]{12,42,32});
+        //list.add(new Integer[]{8,176, null});
+
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> mChildList = new ArrayList<Integer>();
+            row = scanner.nextInt();
+            mChildList.add(row);
+            for (int j = 1; j <= row; ++j) {
+                val = scanner.nextInt();
+                mChildList.add(val);
+            }
+            list.add(mChildList);
+        }
+
+
+        int q;
+        q = scanner.nextInt();
+
+        Integer values [][] = new Integer[1000][2];
+        for (int i = 0; i < q; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                int k;
+                k = scanner.nextInt();
+                values[i][j]=k;
+            }
+        }
+
+
+        for (int i = 0; i < list.size(); i++) {
+
+            for (int j = 1; j < list.get(i).size(); ++j) {
+                System.out.print(list.get(i).get(j)+" ");
+            }
+            System.out.println("");
+
+//            if(list.get(values[i][0]).get(values[i][1]). != null)
+//                System.out.println(list.get(values[i][0]).get(values[i][1]));
+//            else
+//                System.out.println("ERROR!");
+
+        }
+
 
     }
-
 }
